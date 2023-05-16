392d0106cce9212c05f23858c0c4049a96bee61e
RobertJN64 <robertjnies@gmail.com>
2023-05-16 17:07:20 -0400

    add event listener in init py

--- components/gps_ntp/__init__.py
+++ components/gps_ntp/__init__.py
@@ -1,24 +1,28 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.components.gps import gps_ns
+from esphome.components.gps import gps_ns, CONF_GPS_ID, GPS
 
 DEPENDENCIES = ["gps"]
 CODEOWNERS = ["@RobertJN64"]
 
 GPS_NTP = gps_ns.class_("GPS_NTP_Server", cg.Component)
 
-CONF_GPS_ID = "gps_ntp_id"
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
-            cv.GenerateID(): cv.declare_id(GPS_NTP)
+            cv.GenerateID(): cv.declare_id(GPS_NTP),
+            cv.GenerateID(CONF_GPS_ID): cv.use_id(GPS)
         }
     )
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     # https://platformio.org/lib/show/1655/TinyGPSPlus
     cg.add_library("PaulStoffregen/Time", "1.6.1")
+
+    paren = await cg.get_variable(config[CONF_GPS_ID])
+    cg.add(paren.register_listener(var))
